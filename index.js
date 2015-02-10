ef3972e2ca3e2fd51bcc838f2895ec85f2ddad5f
Karim Alibhai <k4rim.sa@gmail.com>
2015-02-10 10:57:47 -0500

    remove incoming number trust (Number.isNumber/areNumbers)

--- index.js
+++ index.js
@@ -28,6 +28,26 @@
             Dataset: require('./lib/dataset.js')
         };
 
+    // extend to detect proper numbers
+    Number.isNumber = function (n) {
+        return n === +n && !isNaN(+n);
+    };
+
+    // solver really only uses the plural
+    // (this is more of a confirm-all rather than
+    //  check plural)
+    Number.areNumbers = function () {
+        var i, args = Array.prototype.slice.apply(arguments, []);
+
+        for (i = 0; i < args.length; i += 1) {
+            if (!Number.isNumber(args[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
     // expose
     data.round = solver.round;
     module.exports = solver;
